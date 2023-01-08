4f98c51df39fe2a7430e90109b347565e9bf67ef
pari3266 <parisatalebi25@gmail.com>
2023-01-08 16:48:41 +0330

    update profile

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit {
-
-  constructor() { }
+  currentUser = {username: '',mobile:'', email: '', password: ''};
+  constructor(
+    private router: Router,
+  ) { }
 
   ngOnInit(): void {
+    this.currentUser = JSON.parse(localStorage.getItem('users') || '{}');
+    console.log(this.currentUser);
+    
   }
 
+  editUser(){
+    localStorage.setItem('users', JSON.stringify(this.currentUser))
+  }
+
+  cancelMembership(){
+    localStorage.removeItem('users');
+    this.router.navigate(['signIn']);
+  }
 }
